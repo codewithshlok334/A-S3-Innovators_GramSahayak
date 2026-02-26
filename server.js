410fd7e8d23f72267c536795802fca53b08df305
codewithshlok334 <pranshmaurya.810@gmail.com>
2026-02-26 14:01:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp"
+});
+
+const result = await model.generateContent("Explain sliding window");
+console.log(result.response.text());
